17f913189b9b291c4050204989b877c048c3da85
nc <31476486+nirc7@users.noreply.github.com>
2022-01-20 11:21:14 +0200

    push timer

---
+++ Lec11 13_01_2022 Camera ImgUpload Push/PushNot Server/WebApplication1/Controllers/PushController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.IO;
+using System.Text;
+using System.Web.Script.Serialization;
+
+namespace WebApplication1.Controllers
+{
+    public class PushController : ApiController
+    {
+        [Route("sendpushnotification")]
+        public string Post([FromBody]PushNotData pnd)
+        {
+            // Create a request using a URL that can receive a post.   
+            WebRequest request = WebRequest.Create("https://exp.host/--/api/v2/push/send");
+            // Set the Method property of the request to POST.  
+            request.Method = "POST";
+            // Create POST data and convert it to a byte array.  
+            var objectToSend = new
+            {
+                to = pnd.to,
+                title = pnd.title,
+                body = pnd.body,
+                badge = pnd.badge,
+                data = pnd.data//new { name = "nir", grade = 100 }
+            };
+
+            string postData = new JavaScriptSerializer().Serialize(objectToSend);
+
+            byte[] byteArray = Encoding.UTF8.GetBytes(postData);
+            // Set the ContentType property of the WebRequest.  
+            request.ContentType = "application/json";
+            // Set the ContentLength property of the WebRequest.  
+            request.ContentLength = byteArray.Length;
+            // Get the request stream.  
+            Stream dataStream = request.GetRequestStream();
+            // Write the data to the request stream.  
+            dataStream.Write(byteArray, 0, byteArray.Length);
+            // Close the Stream object.  
+            dataStream.Close();
+            // Get the response.  
+            WebResponse response = request.GetResponse();
+            // Display the status.  
+            string returnStatus = ((HttpWebResponse)response).StatusDescription;
+            //Console.WriteLine(((HttpWebResponse)response).StatusDescription);
+            // Get the stream containing content returned by the server.  
+            dataStream = response.GetResponseStream();
+            // Open the stream using a StreamReader for easy access.  
+            StreamReader reader = new StreamReader(dataStream);
+            // Read the content.  
+            string responseFromServer = reader.ReadToEnd();
+            // Display the content.  
+            //Console.WriteLine(responseFromServer);
+            // Clean up the streams.  
+            reader.Close();
+            dataStream.Close();
+            response.Close();
+
+            return "success:) --- " + responseFromServer + ", " + returnStatus;
+        }
+
+        [Route("sendpushnotificationFromLocal")]
+        public string Get()
+        {
+            // Create a request using a URL that can receive a post.   
+            WebRequest request = WebRequest.Create("https://exp.host/--/api/v2/push/send");
+            // Set the Method property of the request to POST.  
+            request.Method = "POST";
+            // Create POST data and convert it to a byte array.  
+            var objectToSend = new
+            {
+                to = "ExponentPushToken[I8PGiZHdZMMnFcg_USV5r_]",
+                title = "form local Server",
+                body = "body from local Server",
+                badge = 7,
+                data = new { name = "nir", grade = 100, seconds = DateTime.Now.Second }
+            };
+
+            string postData = new JavaScriptSerializer().Serialize(objectToSend);
+
+            byte[] byteArray = Encoding.UTF8.GetBytes(postData);
+            // Set the ContentType property of the WebRequest.  
+            request.ContentType = "application/json";
+            // Set the ContentLength property of the WebRequest.  
+            request.ContentLength = byteArray.Length;
+            // Get the request stream.  
+            Stream dataStream = request.GetRequestStream();
+            // Write the data to the request stream.  
+            dataStream.Write(byteArray, 0, byteArray.Length);
+            // Close the Stream object.  
+            dataStream.Close();
+            // Get the response.  
+            WebResponse response = request.GetResponse();
+            // Display the status.  
+            string returnStatus = ((HttpWebResponse)response).StatusDescription;
+            //Console.WriteLine(((HttpWebResponse)response).StatusDescription);
+            // Get the stream containing content returned by the server.  
+            dataStream = response.GetResponseStream();
+            // Open the stream using a StreamReader for easy access.  
+            StreamReader reader = new StreamReader(dataStream);
+            // Read the content.  
+            string responseFromServer = reader.ReadToEnd();
+            // Display the content.  
+            //Console.WriteLine(responseFromServer);
+            // Clean up the streams.  
+            reader.Close();
+            dataStream.Close();
+            response.Close();
+
+            return "success:) --- " + responseFromServer + ", " + returnStatus;
+        }
+    }
+
+    public class PushNotData
+    {
+        public string to { get; set; }
+        public string title { get; set; }
+        public string body { get; set; }
+        public int badge { get; set; }
+        public Data data { get; set; }
+    }
+
+    public class Data
+    {
+        public int grade { get; set; }
+        public string name { get; set; }
+    }
+}
